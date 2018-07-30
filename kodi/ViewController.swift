b365c39bb55c2f85177c54142e983b2c7fb7657d
刘佳宁 <2024238027@qq.com>
2018-07-30 10:25:59 +0800

    Initial Commit

---
+++ kodi/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  kodi
+//
+//  Created by 刘佳宁 on 2018/5/20.
+//  Copyright © 2018年 刘佳宁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.test()
+        
+    }
+
+    func test() {
+        
+   print("hello world")
+
+        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
