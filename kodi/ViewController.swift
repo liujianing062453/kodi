2ea5a24ce47f1efe435e8401142f02613f7e1ede
刘佳宁 <2024238027@qq.com>
2019-01-09 16:50:46 +0800

    测试

--- kodi/ViewController.swift
+++ kodi/ViewController.swift
@@ -12,11 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+       
+        print("helllo word")
 
     }
-
-   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
