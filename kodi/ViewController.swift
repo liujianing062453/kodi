b74a0673a9703437a4cf30b1c72a320c9e8e83ee
刘佳宁 <2024238027@qq.com>
2019-01-09 16:57:46 +0800

    test

--- kodi/ViewController.swift
+++ kodi/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        print("helllo word")
+       
 
     }
     override func didReceiveMemoryWarning() {
